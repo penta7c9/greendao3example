e090cf103d872d8ffd84d020ec34c94e60fe0e4d
smartgoat <dmtmd2010@yahoo.com.vn>
2017-04-24 11:20:30 +0700

    init commit

---
+++ app/src/main/java/me/smartgoat/greendao3example/MainActivity.java
@@ -0,0 +1,45 @@
+package me.smartgoat.greendao3example;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_course:
+                    mTextMessage.setText(R.string.title_course);
+                    return true;
+                case R.id.navigation_teacher:
+                    mTextMessage.setText(R.string.title_teacher);
+                    return true;
+                case R.id.navigation_student:
+                    mTextMessage.setText(R.string.title_student);
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
---
+++ app/src/main/java/me/smartgoat/greendao3example/entity/Course.java
@@ -0,0 +1,7 @@
+package me.smartgoat.greendao3example.entity;
+
+//@Entity
+public class Course {
+//    @Id
+    private Long id;
+}
